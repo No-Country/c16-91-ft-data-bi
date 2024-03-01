874248ea2f2aef8133079f7a2098549049621e15
sanabriadiego <sanabria.diego@outlook.com>
2024-02-29 22:23:00 -0400

    Added tables, graphs and prediction functionality. Organized code in folders

--- app.py
+++ app.py
@@ -1,20 +1,42 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
 from prediction import predict
 
+st.title("Machine Learning Analysis")
+
+raw_data = pd.read_csv('./streamlit_data/tables/raw_data.csv')
+st.table(raw_data)
+
+numeric_data = pd.read_csv('./streamlit_data/tables/numeric_data.csv')
+st.table(numeric_data)
+
+st.image('./streamlit_data/img/unshaped_data.png', caption='Data before reshaping')
+st.image('./streamlit_data/img/reshaped_data.png', caption='Data after reshaping')
+
+algorithms_metrics = pd.read_csv('./streamlit_data/tables/algoritms_result.csv')
+st.table(algorithms_metrics)
+
+transactionid = st.number_input('Transaction ID')
+merchantid = st.number_input('Merchant ID')
+transaction_amount = st.number_input('Transaction Amount')
+category = st.selectbox('Category', [0, 1, 2, 3, 4])
+anomaly_score = st.number_input('Anomaly Score')
+amount = st.number_input('Amount')
+customerid = st.number_input('Customer ID')
+age = st.number_input('Age')
+hour = st.number_input('Hour')
 
 
-# st.image('images/titanic.png', caption='Sunrise by the mountains')
+st.write("Es tiempo de probar el rendimiento de nuestro Algortimo, atraves de unas pruebas con casos reales de nuestro dataset")
+st.write("Tenemos eL caso 828 donde el resultado deberia ser 0")
+st.write("Ejemplo numero 1: 829, 2524, 50.962744, 2, 0.838278, 98.143387, 1565, 33, 12")
+st.write("Tambien tenemos el caso 1420 el cual nos deberia dar un resultado 1")
+st.write("Ejemplo numero 2: 150, 2727, 24.965357, 2, 0.084458, 18.914684, 1607, 29, 5")
 
-st.write("Hello World")
 
-# pclass = st.selectbox('What was your passenger class?', [1,2,3])
-# sex = st.selectbox('What is your sex?', [1, 0])
-# sibsp = st.selectbox('Number of siblings aboard', [0,1,2,3])
-# parch = st.selectbox('Number of parents aboard', [0,1,2])
+X = np.array([[transactionid, merchantid, transaction_amount, category, anomaly_score, amount, customerid, age, hour]])
 
-X = np.array([[150.000000, 2727.000000, 24.965357, 2.000000, 0.084458, 18.914684, 1607.000000, 29.000000, 5.000000]])
-
-if st.button('Predict!'):
+if st.button('Obtener Prediccion'):
     result = predict(X)
-    st.text(result[0])
+    st.text(f'El resultado de nuestra prediccion es:{result[0]}')
--- prediction.py
+++ prediction.py
@@ -1,7 +1,7 @@
 import joblib
 
 def predict(data):
-    scaler = joblib.load('scaler.sav')
+    scaler = joblib.load('./streamlit_data/models/scaler.sav')
     norm_data = scaler.transform(data)
-    clf = joblib.load('svm_model.sav')
+    clf = joblib.load('./streamlit_data/models/svm_model.sav')
     return clf.predict(norm_data)
